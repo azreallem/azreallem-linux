910f2b356fbcc6bc4c7b18ab29ef4d789dac2f0f
gaoliang <gaoliang@loongson.cn>
2022-10-18 21:05:01 +0800

    Update

--- scripts/gg.sh
+++ scripts/gg.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-gg=$(grep -Inr --color=auto --include=*.{cpp,c,h,td,inc} $1) 
-echo "$gg"
+
+gg=$(grep -Inr --color=always --include=*.{cpp,c,h,td,inc}  $1 \
+     | awk '{printf("%3d\.\ %s\n",NR,$0)}' \
+     | tee /dev/tty)
+
+echo "=------------------------------------------="
+gg=$(echo "$gg" | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g")
+
 
 read -p "Enter line(1-100, 0 is exit): " LINE
 if [ $LINE -ne 0 ]
 then
-	line_code="$(echo "$gg" | awk '{print $1}' | sed -n ""$LINE"p")"
-	echo $line_code
+	line_code="$(echo "${gg:5}" | awk '{print $1}' | sed -n ""$LINE"p")"
 	lf=${line_code%%:*}
-	echo $lf
 	line_code=${line_code#*:}
 	ll=${line_code%%:*}
-	echo $ll
 	vim  $lf +$ll
 fi
