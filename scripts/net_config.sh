1a5588f6113dd5e20ed249faf74aee0e87442703
gaoliang <azreallem@gmail.com>
2022-10-19 00:42:53 +0800

    Add Open Win11 *.exe Files Scripts In WSL2
    Signed-off-by: gaoliang <azreallem@gmail.com>

--- scripts/net_config.sh
+++ scripts/net_config.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-nohup python3 $HOME/acloong_request.null.py > $HOME/log/net_config.log 2>&1 &
+dir=$HOME/log/tmp
+if [ ! -d $dir ]; then
+  mkdir -p $dir
+fi
+nohup python3 $HOME/acloong_request.null.py > $dir/net_config.log 2>&1 &
---
+++ scripts/win11/vscode.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+dir=$HOME/log/tmp
+if [ ! -d $dir ]; then
+  mkdir -p $dir
+fi
+nohup "/mnt/d/Program Files/Microsoft VS Code/Code.exe" $1 > $dir/vscode.log 2&>1 &
