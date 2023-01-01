84f21a5a14f7c04bc8e2a728754bb68ae3d97b5e
azreallem <azreallem@gmail.com>
2023-01-01 14:33:04 +0000

    Update install-apt-get.sh

--- install-apt-get.sh
+++ install-apt-get.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # modify sources.list of Ubuntu to ustc.edu.cn
-if [ ! -z $(uname -r | grep -i "wsl") ] ; then
-	sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
-fi
+sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 
 # apt-get install
 sudo apt-get update
 sudo apt-get install aptitude zsh vim tmux autojump mate-terminal
-sudo apt-get install gcc g++ gdb python
+sudo apt-get install gcc g++ gdb 
 
 # scripts & backups
 dir=backups_$(date +%y%m%d%H%M%S)
@@ -24,24 +22,18 @@ cp .tmux.conf $HOME
 
 # vim config
 cp .vimrc $HOME
-if [ ! -e $HOME/.vim/bundle/Vundle.vim ]
-then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 source $HOME/.vimrc 2>/dev/null
 echo "=---------- vim install --------------="
 echo "Please waitting ......"
 vim +PluginInstall +qall &> /dev/null &
 echo "=---------- vim finished --------------="
 
-# zsh config
-echo "Please waitting ......"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &> /dev/null
+# oh-my-zsh config
+echo "Please waitting (install oh-my-zsh) ......"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 echo "Install Finished."
-if [ ! -e $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]
-then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-fi
 cp .zshrc $HOME
 source $HOME/.zshrc 2>/dev/null
 zsh
