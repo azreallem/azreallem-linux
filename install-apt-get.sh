3a4ea3d627616db5dc8af7813279c3c8d362b8b7
gaoliang <azreallem@gmail.com>
2023-01-02 07:54:38 +0000

    “Update”

--- install-apt-get.sh
+++ install-apt-get.sh
@@ -8,32 +8,37 @@ sudo apt-get update
 sudo apt-get install aptitude zsh vim tmux autojump mate-terminal
 sudo apt-get install gcc g++ gdb 
 
-# scripts & backups
+# backups
 dir=backups_$(date +%y%m%d%H%M%S)
 mkdir -p $dir
 cp $HOME/.tmux.conf $dir 2>/dev/null
 cp $HOME/.zshrc $dir 2>/dev/null
 cp $HOME/.vimrc $dir 2>/dev/null
 cp -r $HOME/scripts $dir 2>/dev/null
+
+#  copy config & scripts
+cp .tmux.conf $HOME
 cp -r scripts $HOME
 
-# tmux configs
-cp .tmux.conf $HOME
-
 # vim config
-cp .vimrc $HOME
+echo "Please waitting (install vim) ......"
+if [ ! -s $HOME/.vim/bundle/Vundle.vim ]; then
+	rm -rf $HOME/.vim/bundle/Vundle.vim
+fi
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc $HOME
 source $HOME/.vimrc 2>/dev/null
-echo "=---------- vim install --------------="
-echo "Please waitting ......"
 vim +PluginInstall +qall &> /dev/null &
-echo "=---------- vim finished --------------="
+echo "Install Finished."
 
 # oh-my-zsh config
 echo "Please waitting (install oh-my-zsh) ......"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm -rf $HOME/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &
+sleep 5
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-echo "Install Finished."
+wait
 cp .zshrc $HOME
-source $HOME/.zshrc 2>/dev/null
 zsh
+source .zshrc
+echo "Install Finished."
